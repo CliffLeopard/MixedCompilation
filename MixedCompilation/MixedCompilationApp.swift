1254bac706d86d8092ea9acec8f68514aae0bee8
CliffLeoaprd <cliffleopard@yahoo.com>
2022-05-29 20:46:57 +0800

    Initial Commit

---
+++ MixedCompilation/MixedCompilationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixedCompilationApp.swift
+//  MixedCompilation
+//
+//  Created by CliffLeopard on 2022/5/29.
+//
+
+import SwiftUI
+
+@main
+struct MixedCompilationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
